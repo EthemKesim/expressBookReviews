321d182eb24099e16f430f76ae18aa97c9d5c4ef
EthemKesim <kesimethem@gmail.com>
2026-02-10 06:06:09 -0500

    guncelleme 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,12 +15,9 @@ const doesExist = (username)=>{
   }
 }
 
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
   const username = req.body.username;
   const password = req.body.password;
-
 
   if (username && password) {
     if (!doesExist(username)) { 
@@ -33,66 +30,7 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books,null,4));
-});
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const ISBN = req.params.isbn;
-  
-  res.send(books[ISBN])
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  let ans = []
-    for(const [key, values] of Object.entries(books)){
-        const book = Object.entries(values);
-        for(let i = 0; i < book.length ; i++){
-            if(book[i][0] == 'author' && book[i][1] == req.params.author){
-                ans.push(books[key]);
-            }
-        }
-    }
-    if(ans.length == 0){
-        return res.status(300).json({message: "Author not found"});
-    }
-    res.send(ans);
-});
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  let ans = []
-  for(const [key, values] of Object.entries(books)){
-      const book = Object.entries(values);
-      for(let i = 0; i < book.length ; i++){
-          if(book[i][0] == 'title' && book[i][1] == req.params.title){
-              ans.push(books[key]);
-          }
-      }
-  }
-  if(ans.length == 0){
-      return res.status(300).json({message: "Title not found"});
-  }
-  res.send(ans);
-});
-
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  const ISBN = req.params.isbn;
-  res.send(books[ISBN].reviews)
-});
-
-// Task 10 
-// Add the code for getting the list of books available in the shop (done in Task 1) using Promise callbacks or async-await with Axios
-
+// Task 10
 function getBookList(){
   return new Promise((resolve,reject)=>{
     resolve(books);
@@ -102,14 +40,12 @@ function getBookList(){
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   getBookList().then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send("denied")
+    (bk)=>res.status(200).send(JSON.stringify(bk, null, 4)),
+    ()=>res.status(500).json({message:"Error fetching books"})
   );  
 });
 
 // Task 11
-// Add the code for getting the book details based on ISBN (done in Task 2) using Promise callbacks or async-await with Axios.
-
 function getFromISBN(isbn){
   let book_ = books[isbn];  
   return new Promise((resolve,reject)=>{
@@ -125,50 +61,42 @@ function getFromISBN(isbn){
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   getFromISBN(isbn).then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send(error)
+    (bk)=>res.status(200).send(JSON.stringify(bk, null, 4)),
+    (error)=>res.status(404).json({message:error})
   )
- });
+});
 
 // Task 12
-// Add the code for getting the book details based on Author (done in Task 3) using Promise callbacks or async-await with Axios.
-
 function getFromAuthor(author){
-    let output = [];
-    return new Promise((resolve,reject)=>{
-      for (var isbn in books) {
-        let book_ = books[isbn];
-        if (book_.author === author){
-          output.push(book_);
-        }
+  let output = [];
+  return new Promise((resolve,reject)=>{
+    for (var isbn in books) {
+      let book_ = books[isbn];
+      if (book_.author === author){
+        output.push(book_);
       }
-  
-      // 🔴 EKLENEN KISIM
-      if (output.length > 0) {
-        resolve(output);
-      } else {
-        reject("Author not found");
-      }
-    })
-  }
+    }
+    if (output.length > 0) {
+      resolve(output);
+    } else {
+      reject("Author not found");
+    }
+  })
+}
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    getFromAuthor(author)
-      .then(result =>
-        res.status(200).send(JSON.stringify(result, null, 4))
-      )
-      // 🔴 EKLENEN KISIM
-      .catch(error =>
-        res.status(404).json({ message: error })
-      );
-  });
+  const author = req.params.author;
+  getFromAuthor(author)
+    .then(result =>
+      res.status(200).send(JSON.stringify(result, null, 4))
+    )
+    .catch(error =>
+      res.status(404).json({ message: error })
+    );
+});
 
 // Task 13
-// Add the code for getting the book details based on Title (done in Task 4) using Promise callbacks or async-await with Axios.
-
-
 function getFromTitle(title){
   let output = [];
   return new Promise((resolve,reject)=>{
@@ -178,7 +106,11 @@ function getFromTitle(title){
         output.push(book_);
       }
     }
-    resolve(output);  
+    if (output.length > 0) {
+      resolve(output);
+    } else {
+      reject("Title not found");
+    }
   })
 }
 
@@ -186,9 +118,18 @@ function getFromTitle(title){
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
   getFromTitle(title)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
+    .then(result =>
+      res.status(200).send(JSON.stringify(result, null, 4))
+    )
+    .catch(error =>
+      res.status(404).json({message:error})
+    );
+});
+
+// Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  const ISBN = req.params.isbn;
+  res.send(books[ISBN].reviews)
 });
 
 module.exports.general = public_users;
