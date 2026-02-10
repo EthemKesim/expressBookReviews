3318709c09bb96e21b1962056cb837e7eb89b326
EthemKesim <kesimethem@gmail.com>
2026-02-10 06:39:57 -0500

    guncelleme 5 calis artik

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,17 +5,19 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+/**
+ * Check if a username already exists
+ */
 const doesExist = (username)=>{
   let userswithsamename = users.filter((user)=>{
     return user.username === username
   });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
+  return userswithsamename.length > 0;
+};
 
+/**
+ * Register a new user
+ */
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -23,7 +25,9 @@ public_users.post("/register", (req,res) => {
   if (username && password) {
     if (!doesExist(username)) { 
       users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({
+        message: "User successfully registred. Now you can login"
+      });
     } else {
       return res.status(404).json({message: "User already exists!"});
     }
@@ -32,53 +36,75 @@ public_users.post("/register", (req,res) => {
 });
 
 
-// =========================
-// Task 10 – Get all books (Axios + Promise)
-// =========================
+// ======================================================
+// Task 10
+// Get the list of books using Promise callbacks + Axios
+// ======================================================
+
 function getBookList(){
+  // Fetch all books from the API using Axios
   return new Promise((resolve,reject)=>{
     axios.get("http://localhost:5000/")
       .then(response => resolve(response.data))
       .catch(() => reject("Error fetching books"));
-  })
+  });
 }
 
+// Route to return the list of books
 public_users.get('/',function (req, res) {
   getBookList()
-    .then(bk => res.status(200).send(JSON.stringify(bk, null, 4)))
-    .catch(err => res.status(500).json({message: err}));
+    .then(bk =>
+      res.status(200).send(JSON.stringify(bk, null, 4))
+    )
+    .catch(err =>
+      res.status(500).json({message: err})
+    );
 });
 
 
-// =========================
-// Task 11 – Get book by ISBN (Axios + Promise)
-// =========================
+// ======================================================
+// Task 11
+// Get book details by ISBN using Promise callbacks + Axios
+// ======================================================
+
 function getFromISBN(isbn){
+  // Fetch a specific book by ISBN from the API
   return new Promise((resolve,reject)=>{
     axios.get(`http://localhost:5000/isbn/${isbn}`)
       .then(response => resolve(response.data))
       .catch(() => reject("Unable to find book!"));
-  })
+  });
 }
 
+// Route to return book details by ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   getFromISBN(isbn)
-    .then(bk => res.status(200).send(JSON.stringify(bk, null, 4)))
-    .catch(error => res.status(404).json({message:error}));
+    .then(bk =>
+      res.status(200).send(JSON.stringify(bk, null, 4))
+    )
+    .catch(error =>
+      res.status(404).json({message:error})
+    );
 });
 
 
-// =========================
-// Task 12 – Get books by Author (Axios + Promise)
-// =========================
+// ======================================================
+// Task 12
+// Get book details by Author using Promise callbacks + Axios
+// ======================================================
+
 function getFromAuthor(author){
+  // Fetch all books from the API
+  // Filter books based on the provided author name
+  // Resolve with matching books or reject if none found
   return new Promise((resolve,reject)=>{
     axios.get("http://localhost:5000/")
       .then(response => {
         let output = [];
         let booksData = response.data;
 
+        // Loop through all books and match author name
         for (let isbn in booksData) {
           if (booksData[isbn].author === author) {
             output.push(booksData[isbn]);
@@ -92,9 +118,10 @@ function getFromAuthor(author){
         }
       })
       .catch(() => reject("Error fetching books"));
-  })
+  });
 }
 
+// Route to return books by author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
   getFromAuthor(author)
@@ -107,16 +134,21 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 
-// =========================
-// Task 13 – Get books by Title (Axios + Promise)
-// =========================
+// ======================================================
+// Task 13
+// Get book details by Title using Promise callbacks + Axios
+// ======================================================
+
 function getFromTitle(title){
+  // Fetch all books from the API
+  // Filter books based on the provided title
   return new Promise((resolve,reject)=>{
     axios.get("http://localhost:5000/")
       .then(response => {
         let output = [];
         let booksData = response.data;
 
+        // Loop through all books and match title
         for (let isbn in booksData) {
           if (booksData[isbn].title === title) {
             output.push(booksData[isbn]);
@@ -130,9 +162,10 @@ function getFromTitle(title){
         }
       })
       .catch(() => reject("Error fetching books"));
-  })
+  });
 }
 
+// Route to return books by title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
   getFromTitle(title)
@@ -145,12 +178,13 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 
-// =========================
+// ======================================================
 // Get book review (unchanged)
-// =========================
+// ======================================================
+
 public_users.get('/review/:isbn',function (req, res) {
   const ISBN = req.params.isbn;
-  res.send(books[ISBN].reviews)
+  res.send(books[ISBN].reviews);
 });
 
 module.exports.general = public_users;
